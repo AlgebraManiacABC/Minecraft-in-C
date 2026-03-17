d86e8a7bac437c1c58958dc049628c8ec631d7c0
Arthur Schieszer <67496505+TheDeadCell@users.noreply.github.com>
2026-03-17 16:35:32 -0500

    Translate SetNormal, SetSetUV, and SetColor from C++ to C.

--- voxel_mesh.c
+++ voxel_mesh.c
@@ -157,9 +157,10 @@ void AddCube(Vector3 position, bool faces[6], int block){
 	}
 }
 
-inline void SetNormal(Vector3& value) { Normal = value; }
-inline void SetNormal(float x, float y, float z) { Normal = Vector3{ x,y,z }; }
-inline void SetSetUV(Vector2& value) { UV = value; }
-inline void SetSetUV(float x, float y ) { UV = Vector2{ x,y }; }
+inline void SetNormalV(Vector3 value) { Normal = value; }
+inline void SetNormal(float x, float y, float z) { Normal = (Vector3){ x,y,z }; }
 
-inline void SetColor(Color& value) { VertColor = value; }
+inline void SetSetUVV(Vector2 value) { UV = value; }
+inline void SetSetUV(float x, float y ) { UV = (Vector2){ x,y }; }
+
+inline void SetColor(Color value) { VertColor = value; }
