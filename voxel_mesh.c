c106e6b342dc6945db5c993a8bfc22d4f9c51434
Arthur Schieszer <67496505+TheDeadCell@users.noreply.github.com>
2026-03-17 16:24:07 -0500

    Convert AddCube from C++ to C

--- voxel_mesh.c
+++ voxel_mesh.c
@@ -1,3 +1,6 @@
+#include "raylib.h"
+#include "stdbool.h"
+
 InitVoxelMesh(int triangles, bool useColors = false){
 
 }
@@ -6,8 +9,152 @@ PushVertex(Vector3& vertex, float xOffset = 0, float yOffset = 0, float zOffset 
 
 }
 
-AddCube(Vector3&& position, bool faces[6], int block){
+void AddCube(Vector3 position, bool faces[6], int block){
+	Rectangle* uvRect = &BlockColors[block];
 
+	SetColor(BlockTints[block]);
+
+	SetSetUV(0,0);
+	//z-
+	if (faces[NorthFace])
+	{
+		SetNormal( 0, 0, -1 );
+		SetSetUV(uvRect->width, uvRect->height);
+		PushVertex(position);
+
+		SetSetUV(uvRect->x, uvRect->y);
+		PushVertex(position, 1, 1, 0);
+
+		SetSetUV(uvRect->x, uvRect->height);
+		PushVertex(position, 1, 0, 0);
+
+		SetSetUV(uvRect->width, uvRect->height);
+		PushVertex(position);
+
+		SetSetUV(uvRect->width, uvRect->y);
+		PushVertex(position, 0, 1, 0);
+
+		SetSetUV(uvRect->x, uvRect->y);
+		PushVertex(position, 1, 1, 0);
+	}
+
+	// z+
+	if (faces[SouthFace])
+	{
+		SetNormal(0, 0 ,1);
+
+		SetSetUV(uvRect->width, uvRect->height);
+		PushVertex(position, 0, 0, 1);
+
+		SetSetUV(uvRect->x, uvRect->height);
+		PushVertex(position, 1, 0, 1);
+
+		SetSetUV(uvRect->x, uvRect->y);
+		PushVertex(position, 1, 1, 1);
+
+		SetSetUV(uvRect->width, uvRect->height);
+		PushVertex(position, 0, 0, 1);
+
+		SetSetUV(uvRect->x, uvRect->y);
+		PushVertex(position, 1, 1, 1);
+
+		SetSetUV(uvRect->width, uvRect->y);
+		PushVertex(position, 0, 1, 1);
+	}
+
+	// x+
+	if (faces[WestFace])
+	{
+		SetNormal(1, 0, 0 );
+		SetSetUV(uvRect->x, uvRect->height);
+ 			PushVertex(position, 1, 0, 1);
+
+		SetSetUV(uvRect->width, uvRect->height);
+		PushVertex(position, 1, 0, 0);
+
+		SetSetUV(uvRect->width, uvRect->y);
+ 			PushVertex(position, 1, 1, 0);
+
+		SetSetUV(uvRect->x, uvRect->height);
+ 			PushVertex(position, 1, 0, 1);
+
+		SetSetUV(uvRect->width, uvRect->y);
+ 			PushVertex(position, 1, 1, 0);
+
+		SetSetUV(uvRect->x, uvRect->y);
+ 			PushVertex(position, 1, 1, 1);
+	}
+
+	// x-
+	if (faces[EastFace])
+	{
+		SetNormal(-1, 0, 0);
+
+		SetSetUV(uvRect->width, uvRect->height);
+		PushVertex(position, 0, 0, 1);
+
+		SetSetUV(uvRect->x, uvRect->y);
+		PushVertex(position, 0, 1, 0);
+
+		SetSetUV(uvRect->x, uvRect->height);
+		PushVertex(position, 0, 0, 0);
+
+		SetSetUV(uvRect->width, uvRect->height);
+		PushVertex(position, 0, 0, 1);
+
+		SetSetUV(uvRect->width, uvRect->y);
+		PushVertex(position, 0, 1, 1);
+
+		SetSetUV(uvRect->x, uvRect->y);
+		PushVertex(position, 0, 1, 0);
+	}
+
+	if (faces[UpFace])
+	{
+		SetNormal(0, 1, 0 );
+
+		SetSetUV(uvRect->x, uvRect->y);
+		PushVertex(position, 0, 1, 0);
+
+		SetSetUV(uvRect->width, uvRect->height);
+		PushVertex(position, 1, 1, 1);
+
+		SetSetUV(uvRect->width, uvRect->y);
+		PushVertex(position, 1, 1, 0);
+
+		SetSetUV(uvRect->x, uvRect->y);
+		PushVertex(position, 0, 1, 0);
+
+		SetSetUV(uvRect->x, uvRect->height);
+		PushVertex(position, 0, 1, 1);
+
+		SetSetUV(uvRect->width, uvRect->height);
+		PushVertex(position, 1, 1, 1);
+	}
+
+	SetSetUV(0, 0);
+	if (faces[DownFace])
+	{
+		SetNormal(0, -1, 0);
+
+		SetSetUV(uvRect->x, uvRect->y);
+		PushVertex(position, 0, 0, 0);
+
+		SetSetUV(uvRect->width, uvRect->y);
+		PushVertex(position, 1, 0, 0);
+
+		SetSetUV(uvRect->width, uvRect->height);
+		PushVertex(position, 1, 0, 1);
+
+		SetSetUV(uvRect->x, uvRect->y);
+		PushVertex(position, 0, 0, 0);
+
+		SetSetUV(uvRect->width, uvRect->height);
+		PushVertex(position, 1, 0, 1);
+
+		SetSetUV(uvRect->x, uvRect->height);
+		PushVertex(position, 0, 0, 1);
+	}
 }
 
 inline void SetNormal(Vector3& value) { Normal = value; }
